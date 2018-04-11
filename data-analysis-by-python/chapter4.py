e891745639e7f23b6ee79c589639163dcab0c9cd
Matbird <matbird@126.com>
2018-04-11 16:21:23 +0800

    numpy操作数组的一些函数练习

--- data-analysis-by-python/chapter4.py
+++ data-analysis-by-python/chapter4.py
@@ -67,5 +67,68 @@ if __name__ == '__main__':
 
     # 切片操作对原始的数组起作用,处理大数据时候多次复制的开销很大
 
+    # 布尔型索引
+    # names = np.array(['Bob','Joe','Will','Bob','Will','Joe','Joe'])
+    # data = randn(7,4)
+    # print(names)
+    # print(data)
+    # print(names == 'Bob')
+    # print(data[names == 'Bob'])
+    # print(data[names == 'Bob',2:])
+    # print(data[names == 'Bob',3])
+
+    # 花式索引:传入一个数组作为索引
+    # arr = np.empty((8,4))
+    # for i in range(8):
+    #     arr[i] = i
+    # print(arr)
+    # print(arr[[4,3,0,6]])
+
+    # 传入多个数组作为索引,返回的是一个一维数组
+    # arr = np.arange(32).reshape((8,4))
+    # print(arr)
+    # print(arr[[1,5,7,2],[0,3,1,2]])
+
+    # 花式索引跟切片不同,它总是将数据复制到新数组中
+
+    # 数据的转置和轴对换
+    # arr = np.arange(15).reshape((3,5))
+    # print(arr)
+    # print(arr.T)
+    # print(arr)
+
+    # dot 计算内积 X^T * X
+    # arr = randn(6,3)
+    # print(arr)
+    # print(np.dot(arr,arr.T))
+
+    # print(np.arange(12).reshape((3,4)))
+
+    # print(np.arange(12) * 2)
+
+    # 元素级数组函数
+    # 一元ufunc
+    # print(np.sqrt(np.arange(5)))
+    # print(np.modf(np.arange(10)))
+    # 二元ufunc
+    # print(np.mod(np.arange(6,11) * 3,np.arange(1,6)))
+
+    # arr = randn(5,4)
+    # print(arr)
+    # arr.sort(0)
+    # print(arr)
+
+    # 唯一化,unique
+    # names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+    # print(np.unique(names))
+    # values = np.array([2,3,6,1,0,5,5])
+    # print(np.in1d(values,[2,3,6]))
+
+    # 数组文件的保存输入输出
+    # arr = np.arange(10)
+    # np.save('some_arr',arr)
+    # print(np.load('some_arr.npy'))
+    # arr = randn(5,4)
+    # np.savetxt('some_arr_txt',arr)
 
     pass
